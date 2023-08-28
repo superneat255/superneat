23596f82641dacbc8982d25fe7f612b8a557b601
Immanuel Pishon <77734719+superneat255@users.noreply.github.com>
2023-08-28 17:58:15 +0300

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,1 +1,55 @@
+import os
+import errno
+import signal
+from functools import wraps
 
+
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wraps(func)(wrapper)
+
+    return decorator
+
+
+
+def runSh(args, *, output=False, shell=False, cd=None):
+    import subprocess, shlex
+
+    if not shell:
+        if output:
+            proc = subprocess.Popen(
+                shlex.split(args), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=cd
+            )
+            while True:
+                output = proc.stdout.readline()
+                if output == b"" and proc.poll() is not None:
+                    return
+                if output:
+                    print(output.decode("utf-8").strip())
+        return subprocess.run(shlex.split(args), cwd=cd).returncode
+    else:
+        if output:
+            return (
+                subprocess.run(
+                    args,
+                    shell=True,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.STDOUT,
+                    cwd=cd,
+                )
+                .stdout.decode("utf-8")
+                .strip()
+            )
+        return subprocess.run(args, shell=True, cwd=cd).returncode
